3b03ef663dd3ade8bf6ecf279327f32bfcf0d58a
ŞEHMUS EKİN <146784285+sehmusekin@users.noreply.github.com>
2024-06-02 15:05:57 +0300

    iki işlemli hesap makinesi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,57 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double a,b;
+        char islem;
+
+        System.out.println("İki işlemli Hesap Makinesi");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Birinci Sayıyı Giriniz :");
+        a = input.nextDouble();
+
+        System.out.print("İkinci Sayıyı Giriniz :");
+        b = input.nextDouble();
+
+        System.out.print("Yapılacak işlemi seçin (+, -, *, /): ");
+        islem =input.next().charAt(0);
+
+        double result;
+
+
+        switch (islem){
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a-b;
+                break;
+            case'*':
+                result = a * b;
+                break;
+            case '/':
+                if (b != 0) {
+                    result = a / b;
+                } else {
+                    System.out.println("Hata: Sıfıra bölme tanımsızdır.");
+                    return;
+                }
+                break;
+
+            default:
+                System.out.print("Hata: Geçersiz işlem.");
+                return;
+        }
+
+        System.out.println("Sonuç: " + result);
+
+
+
+
+
     }
 }
